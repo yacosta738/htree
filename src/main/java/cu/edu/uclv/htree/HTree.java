0bb2e10c55712c9b05ff0cb1c6a3010158009f01
yuniel.acosta <yuniel.acosta@outlook.com>
2018-09-18 09:25:56 -0400

    Project Structure with HTree class

---
+++ src/main/java/cu/edu/uclv/htree/HTree.java
@@ -0,0 +1,9 @@
+package cu.edu.uclv.htree;
+
+/**
+ * @author yuniel acosta, anakarla sotolongo, elvis hernandez y danay
+ * Created in 18/9/2018 09:24
+ * @project htree
+ **/
+public class HTree {
+}
